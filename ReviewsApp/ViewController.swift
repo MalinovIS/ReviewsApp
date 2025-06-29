7214c0f57e4384d8a9ad4c38dcb1b2907c25f54a
MalinovIS <49347069+MalinovIS@users.noreply.github.com>
2025-06-30 01:24:08 +0400

    Initial Commit

---
+++ ReviewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReviewsApp
+//
+//  Created by Илья Малинов on 30.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
